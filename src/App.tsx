561f45d66662fb9500cd76bd6b856ff14adb41a8
Muhammad Zachrie Kurniawan <zachrie2005@gmail.com>
2023-01-28 19:22:13 +0700

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [currentUrl, setCurrentUrl] = useState<string>('');
+  const [url, setUrl] = useState<string>('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrentUrl(e.target.value);
+  };
+
+  const handleClick = () => {
+    if (currentUrl !== '') setUrl(currentUrl);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="h-screen flex flex-col justify-center items-center">
+      <div className="container mx-auto h-full p-10">
+        <div className="flex mb-20">
+          <input type="text" className="form-input px-4 py-3 w-full rounded-l-xl" onChange={handleChange} />
+          <button className="py-2 px-3 bg-pink-500 hover:bg-pink-600 text-white rounded-r-xl transition-all" onClick={handleClick}>
+            Convert
+          </button>
+        </div>
+        <iframe
+          title="widget"
+          id="widgetv2Api"
+          src={`https://convert2mp3s.com/api/widgetv2?url=${url ? url : 'https://www.youtube.com/watch?v=pRpeEdMmmQ0'}`}
+          width="100%"
+          height="100%"
+          style={{ border: 'none' }}
+        ></iframe>
+      </div>
+    </section>
   );
-}
+};
 
 export default App;
